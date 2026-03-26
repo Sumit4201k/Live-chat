12bfd3f88644c15929882afd17c3a7f10ab3b728
Sumit4201k <kamtisumit6856@gmail.com>
2026-03-26 21:29:01 +0530

    feat: add live-chat package and configure frontend and backend dependencies

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,16 +1,28 @@
 import express from "express";
 import dotenv from "dotenv"
 import AuthRouter from "./routes/auth.route.js";
+import path from "path"
 
 dotenv.config()
 
 const app = express();
+const __dirname = path.resolve()
 
 const PORT = process.env.PORT || 3000;
 
-console.log( PORT);
+console.log( PORT , process.env.NODE_ENV);
 
 app.use("/api/auth" , AuthRouter)
+
+if (process.env.NODE_ENV === "production") {
+
+    app.use(express.static(path.join(__dirname,"../Frontend/dist")))
+
+    app.get("*",(_,res) =>{
+        res.sendFile(path.join(__dirname,"../Frontend","dist","index.html"))
+    })
+    
+}
 
 app.listen(PORT,()=>{
 
