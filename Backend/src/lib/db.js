a47b47d5024581903f755797942750b42814a6c0
Sumit4201k <kamtisumit6856@gmail.com>
2026-03-31 22:41:31 +0530

    feat: implement email functionality with welcome email template and resend integration

--- Backend/src/lib/db.js
+++ Backend/src/lib/db.js
@@ -1,6 +1,12 @@
 import mongoose from 'mongoose';
 
 export const connectDB = async() => {
+
+    const MOGODB= process.env.MONGODB_URL
+    if (!MOGODB) {
+        console.log("MONGO DB URL ERROR ");
+        
+    }
     try {
         const conn = await mongoose.connect(process.env.MONGODB_URL)
         console.log("DB CONNECTED :",conn.connection.host);
@@ -16,14 +22,13 @@ export const connectDB = async() => {
 /*
 
 Self note: When connecting to MongoDB, you might see a connection string like this:
-⚙️ 2. ?retryWrites=true
+ 2. ?retryWrites=true
 
-👉 This means:
-
-If a write operation fails (like insert/update)
+ This means:
+ If a write operation fails (like insert/update)
 MongoDB will automatically retry it
 
-💡 Example:
+ Example:
 
 Network glitch happens while saving message
 MongoDB retries → avoids data loss
@@ -31,14 +36,13 @@ MongoDB retries → avoids data loss
 ✔ Improves reliability
 ✔ Default in Atlas (you should keep it)
 
-🧠 3. &w=majority
-
-👉 This is about write safety
+ 3. &w=majority
+ This is about write safety
 
 w = write concern
 majority = data is saved only when most servers confirm it
 
-💡 Example:
+ Example:
 
 Your cluster has 3 nodes
 At least 2 must confirm the write
